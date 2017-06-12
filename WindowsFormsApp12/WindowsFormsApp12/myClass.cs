6d80e7385f92b0050bf158e204b7d3aef0255ce4
Mike <bananas@someplace.com>
2017-06-12 14:36:27 -0400

    17 C# Practice Code

---
+++ WindowsFormsApp12/WindowsFormsApp12/myClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNameSpace
+{
+    class myClass
+    {
+        string name;
+        public myClass(string name)
+        {
+            Name = name;
+        }
+
+        public myClass()
+        {
+                
+        }
+    }
+}

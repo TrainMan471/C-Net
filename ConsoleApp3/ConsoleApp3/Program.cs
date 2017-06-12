0575aaf12db975657aa21037e4e766bb71200d68
Mike <bananas@someplace.com>
2017-06-12 14:12:45 -0400

    Third C# File/Third Practice Application

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = string.Empty;
+            Console.WriteLine("Enter a String");
+            a = Console.ReadLine();
+
+            char[] temp = a.ToCharArray();
+            Array.Reverse(temp);
+            string b = new string(temp);
+
+            if (a.Equals(b)) 
+            {
+                Console.WriteLine("palindrome");
+            }
+            else 
+            {
+                Console.WriteLine("Not a PalinDrome");
+            }
+        }
+    }
+}
